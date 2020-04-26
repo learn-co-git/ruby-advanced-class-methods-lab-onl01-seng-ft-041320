6c008437c5c2d6ef3cf9846dccfba8417eb38c12
Neil Empey <argus.two.2@gmail.com>
2020-04-26 05:41:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,11 +21,6 @@ class Song
      return song
 
   end
-  
-    def self.find_by_name(name)
-     return self.all.select { |song| song.name == name }[0]
-     return self.all.find { |song| song.name == name }
-    end
    
      def self.find_or_create_by_name(name)
       return self.find_by_name(name) || self.create_by_name(name)
